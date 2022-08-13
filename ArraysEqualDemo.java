3fcdc6d5e2e11007869cd5b550f87f939be920d7
shanid544 <shanidvpputhanpeedika@gmail.com>
2022-08-13 09:39:37 -0700

    Add files via upload

---
+++ ArraysEqualDemo.java
@@ -0,0 +1,16 @@
+class ArraysEqualDemo {
+    public static void main(String[] args) {
+	
+        String[] a={"shanid","thanveer","jaza","aaban",
+		"hafeefa","khadeeja","moosa"};
+		
+		String[] c={"shanid","thanveer","jaza","aaban",
+		"hafeefa","khadeeja","moosa"};
+		
+		boolean b = java.util.Arrays.equals(a,c);
+		
+		System.out.println(b);
+		
+		
+    }
+}
